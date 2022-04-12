77e828ad4ab4feb2938040a31aca809ed338ef29
Mike M <51172274+MichaelCMooney@users.noreply.github.com>
2022-04-12 08:52:46 -0400

    Add files via upload

---
+++ TailAllLogs.ps1
@@ -0,0 +1,13 @@
+﻿Workflow My-Tail
+{
+
+    Param([string[]] $Path)
+
+    foreach -parallel ($file in $path)
+    {
+        Get-Content -Path $file -Tail 1 -Wait
+    }
+
+}
+
+My-Tail (dir C:\OMAPerformanceTools\PerfTest\v4\omatest\logs\*.log)
